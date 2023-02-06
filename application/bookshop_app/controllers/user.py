402a8aa4fb0a5b10693133c0e608348df1a4fb9d
Andrei Zaneuski <andrei_zaneuski1@epam.com>
2023-02-06 01:15:27 +0100

    feat: add booking, product, store controllers

--- application/bookshop_app/controllers/user.py
+++ application/bookshop_app/controllers/user.py
@@ -1,33 +1,38 @@
-"""Controller module."""
+"""User controller"""
+from flask import Response
 
 from bookshop_app.authenticator import auth
 from bookshop_app.services.user import UserService
 
 
 class UserController:
+    """Controller for User"""
 
     @staticmethod
-    def get(user_id: int):
-        """Get user resource."""
+    @auth.login_required
+    def get(user_id: int) -> dict:
+        """Get user resource by user id"""
         return UserService.get(user_id)
 
     @staticmethod
     @auth.login_required
-    def get_all():
-        """Get all user resources."""
+    def get_all() -> list[dict]:
+        """Get all user resources"""
         return UserService.get_all()
 
     @staticmethod
-    def create():
-        """Create user resource."""
+    def create() -> Response | tuple[dict, int]:
+        """Create user resource"""
         return UserService.create()
 
     @staticmethod
-    def delete(user_id: int):
+    @auth.login_required
+    def delete(user_id: int) -> tuple[dict[str, str], int]:
         """Delete user resource"""
         return UserService.delete(user_id)
 
     @staticmethod
-    def update(user_id: int):
+    @auth.login_required
+    def update(user_id: int) -> Response | tuple[dict, int]:
         """Update user resource"""
         return UserService.update(user_id)
