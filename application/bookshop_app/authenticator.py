88bd1d82be95befb73c901a929f7f7055826c8b9
Andrei Zaneuski <andrei_zaneuski1@epam.com>
2023-02-11 23:33:37 +0100

    fix: use correct user model name
    Changes:
    - Use correct name of the User ORM model
    - Add docs

--- application/bookshop_app/authenticator.py
+++ application/bookshop_app/authenticator.py
@@ -1,14 +1,20 @@
+"""Authenticator module"""
 from flask_httpauth import HTTPBasicAuth
 
-from bookshop_app.models.user_model import User
+from bookshop_app.models.user import UserModel
 
 auth = HTTPBasicAuth()
 
 
 @auth.verify_password
-def verify_password(login, password):
-    # Checks login/password pair for authentication
-    user = User.query.filter_by(login=login).first()
+def verify_password(login: str, password: str) -> bool:
+    """Verify given password for given login
+
+    :param login: user login
+    :param password: user login for password to verify
+    :return: True if given password valid for given login else False
+    """
+    user = UserModel.query.filter_by(login=login).first()
     if not user or not user.verify_password(password):
         return False
     return True
