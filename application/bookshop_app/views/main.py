94aa1b351b247e749b92a4fe5e415fe210202a7a
Gliger13 <zanevskiyandrey@gmail.com>
2023-02-25 23:56:19 +0100

    feat(app): add booshop blueprints

---
+++ application/bookshop_app/views/main.py
@@ -0,0 +1,27 @@
+"""Main pages views
+
+Module contains routes for the main blueprint, which includes basic
+information, home pages.
+"""
+from flask import Blueprint, render_template
+
+__all__ = ["main_blueprint"]
+
+main_blueprint = Blueprint(
+    name="main_blueprint",
+    import_name=__name__,
+    static_folder="static",
+    template_folder="templates"
+)
+
+
+@main_blueprint.route('/')
+def home_page():
+    """Default route for home page"""
+    return render_template("main/home.html")
+
+
+@main_blueprint.route('/about')
+def about_page():
+    """Default route for home page"""
+    return render_template("main/about.html")
