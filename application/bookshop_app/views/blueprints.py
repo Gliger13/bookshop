9a11997866ab0f1b1eb8d2eb8df5ef3af29890e9
Andrei Zaneuski <andrei_zaneuski1@epam.com>
2023-02-26 16:36:44 +0100

    feat(app): add blueprint and route for media dir

--- application/bookshop_app/views/blueprints.py
+++ application/bookshop_app/views/blueprints.py
@@ -8,6 +8,7 @@ from flask import Flask
 from bookshop_app.views.authentication import authentication_blueprint
 from bookshop_app.views.bookings import bookings_blueprint
 from bookshop_app.views.main import main_blueprint
+from bookshop_app.views.media import media_blueprint
 from bookshop_app.views.products import products_blueprint
 from bookshop_app.views.store_items import store_items_blueprint
 from bookshop_app.views.users import users_blueprint
@@ -26,3 +27,4 @@ def register_all_blueprints(application: Flask) -> None:
     application.register_blueprint(products_blueprint)
     application.register_blueprint(bookings_blueprint)
     application.register_blueprint(store_items_blueprint)
+    application.register_blueprint(media_blueprint)
