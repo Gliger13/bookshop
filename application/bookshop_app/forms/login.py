d174526433f01f7794768385e080916e9fd1f989
Gliger13 <zanevskiyandrey@gmail.com>
2023-02-26 17:45:49 +0100

    feat(app): fill login page

--- application/bookshop_app/forms/login.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
-
-
-class LoginForm(FlaskForm):
-    login = StringField("login", validators=[validators.input_required(), validators.length(max=256)])
-    password = PasswordField("password")
