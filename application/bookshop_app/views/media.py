43f6868ec62c1f3aadb3bbe36616b9f647b21465
Gliger13 <zanevskiyandrey@gmail.com>
2023-02-26 16:40:21 +0100

    docs(app): add typing and docstrings for media

--- application/bookshop_app/views/media.py
+++ application/bookshop_app/views/media.py
@@ -1,4 +1,10 @@
+"""Media views
+
+Module contains routes for the media blueprint, which includes loading files
+from the media folder.
+"""
 from flask import Blueprint, send_from_directory
+from requests import Response
 
 media_blueprint = Blueprint(
     name="media_blueprint",
@@ -8,6 +14,6 @@ media_blueprint = Blueprint(
 
 
 @media_blueprint.route('/media/<path>', methods=["GET"])
-def get_media(path: str):
+def get_media(path: str) -> Response:
     """Route for any media file"""
     return send_from_directory(directory="media", path=path)
