8a4d5db095f75ca050df0b532656e8b7eee1f903
Gliger13 <zanevskiyandrey@gmail.com>
2023-03-01 00:10:13 +0100

    fix(app): configure proper sign up
    - Use API for the registration
    - Remove repeat password field
    - Use same template parts for login and registration

--- application/bookshop_app/static/js/authentication.js
+++ application/bookshop_app/static/js/authentication.js
@@ -1,3 +1,8 @@
+function display_error(error) {
+    $("#error-message-banner").removeClass("opacity-0").addClass("opacity-85");
+    $("#error-message").html(error);
+}
+
 function login_user(data) {
     $.ajax('/login', {
         data: JSON.stringify(data),
@@ -10,30 +15,69 @@ function login_user(data) {
             window.location.href = data;
         },
         error: function(request, status, error) {
-            alert("Authentication failed, please try again.");
+            display_error("Can not login user. Something went wrong (┳‸┳)");
         }
     });
 }
 
+function authenticate(login, password) {
+    var authData = btoa(unescape(encodeURIComponent(`${login}:${password}`)))
+    $.ajax('/api/generate_token', {
+        contentType: 'application/json',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', `Basic ${authData}`);
+        },
+        type: 'GET',
+        success: function(data) {
+            window.localStorage.setItem('AuthToken', data.AuthToken);
+            login_user({'login': login});
+        },
+        error: function(request, status, error) {
+            display_error("Invalid credentials");
+        }
+    });
+}
 
 $(document).ready(function(){
     $("#login-form").on("submit", function (e) {
         e.preventDefault();
         var login = $(this).find('#login').val();
         var password = $(this).find('#password').val()
-        var authData = btoa(unescape(encodeURIComponent(`${login}:${password}`)))
-        $.ajax('/api/generate_token', {
+        authenticate(login, password);
+    });
+    $("#registration-form").on("submit", function (e) {
+        e.preventDefault();
+        var login = $(this).find('#login').val();
+        var password = $(this).find('#password').val();
+        var request_json = {
+            "login": login,
+            "password": password,
+            "email": $(this).find('#email').val()
+        };
+        var name = $(this).find('#name').val();
+        if (name != "" ) {
+            request_json["name"] = name
+        }
+
+        var phone = $(this).find('#phone').val();
+        if (phone != "" ) {
+            request_json["phone"] = phone
+        }
+
+        var address = $(this).find('#address').val();
+        if (address != "" ) {
+            request_json["address"] = address
+        }
+
+        $.ajax('/api/user', {
             contentType: 'application/json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', `Basic ${authData}`);
-            },
-            type: 'GET',
-            success: function(data) {
-                window.localStorage.setItem('AuthToken', data.AuthToken);
-                login_user({'login': login})
+            data: JSON.stringify(request_json),
+            type: 'POST',
+            success: function() {
+                authenticate(login, password);
             },
             error: function(request, status, error) {
-                alert("Authentication failed, please try again.");
+                display_error(jQuery.parseJSON(request.responseText).error);
             }
         });
     });
--- application/bookshop_app/views/authentication.py
+++ application/bookshop_app/views/authentication.py
@@ -6,9 +6,7 @@ functionality of user registration, login, log out functionality.
 
 from flask import Blueprint, redirect, render_template, request, Response, url_for
 from flask_login import current_user, login_required, login_user, logout_user
-from requests import codes
 
-from bookshop_app.controllers.user import UserController
 from bookshop_app.forms.authentication import LoginForm, RegistrationForm
 from bookshop_app.services.authentication import load_login_user
 
@@ -33,15 +31,16 @@ def login_page() -> str:
 def login_post():
     """Route for the POST request to the login endpoint"""
     bearer = request.headers.get("Authorization")
+    login_form = LoginForm()
     if not bearer:
         error = "Authorization header was not provided"
-        render_template("authentication/login.html", user=current_user, error=error)
+        render_template("authentication/login.html", user=current_user, form=login_form, error=error)
 
     token = bearer.split()[1]
     user = load_login_user(token)
     if not user:
         error = "Can not authenticate with provided authorization"
-        return render_template("authentication/login.html", user=current_user, error=error)
+        return render_template("authentication/login.html", user=current_user, form=login_form, error=error)
 
     login_user(user, remember=True)
     return url_for("products_blueprint.products_page")
@@ -68,11 +67,4 @@ def registration_post() -> Response | str:
     registration_form = RegistrationForm()
     if not registration_form.validate_on_submit():
         return render_template("authentication/registration.html", user=current_user, form=registration_form)
-
-    create_user_response_data, status_code = UserController.create()
-    if status_code is not codes.created:
-        error = create_user_response_data.json.get("error", "Failed to create new user")
-        return render_template("authentication/registration.html",
-                               user=current_user, form=registration_form, error=error)
-
-    return redirect(url_for("products_blueprint.products_page"))
+    return login_post()
