3ca5a953d24801e3629ee96e572c065bc696eec9
Gliger13 <zanevskiyandrey@gmail.com>
2023-03-05 22:19:36 +0100

    feat(app): fill product page
    - Fill page for the single product
    - Use elements folder for product templates
    - Set default booking status id to 1
    - Remove book button from the small product card

--- application/bookshop_app/views/products.py
+++ application/bookshop_app/views/products.py
@@ -7,7 +7,9 @@ from flask import Blueprint, render_template
 from flask_login import current_user
 
 from bookshop_app.controllers.product import ProductController
-from bookshop_app.forms.product import CreateProductForm, DeleteProductForm, UpdateProductForm
+from bookshop_app.forms.booking import BookProductForm
+from bookshop_app.forms.product import CreateProductForm, DeleteProductByIdForm, DeleteProductForm, \
+    UpdateProductByIdForm, UpdateProductForm
 from bookshop_app.views.main import main_blueprint
 
 __all__ = ["products_blueprint"]
@@ -38,7 +40,31 @@ def products_page() -> str:
     )
 
 
-@products_blueprint.route("/product/<product_id>", methods=["GET"])
+@products_blueprint.route("/product/<product_id>/", methods=["GET"])
 def product_page(product_id: str) -> str:
     """Route for the GET request to the product endpoint"""
-    return render_template("products/product.html", user=current_user)
+    product_id = int(product_id)
+    product, status_code = ProductController.get(product_id)
+
+    book_product_by_id_form = BookProductForm()
+    book_product_by_id_form.product_id.data = product_id
+    book_product_by_id_form.user_id.data = current_user.id
+    book_product_by_id_form.delivery_address.data = current_user.address
+
+    update_product_by_id_form = UpdateProductByIdForm()
+    update_product_by_id_form.id.data = product_id
+    update_product_by_id_form.name.data = product["name"]
+    update_product_by_id_form.author.data = product["author"]
+    update_product_by_id_form.description.data = product["description"]
+    update_product_by_id_form.price.data = product["price"]
+
+    delete_product_by_id_form = DeleteProductByIdForm()
+    delete_product_by_id_form.id.data = product_id
+    return render_template(
+        template_name_or_list="products/product.html",
+        product=product,
+        user=current_user,
+        book_product_by_id_form=book_product_by_id_form,
+        update_product_by_id_form=update_product_by_id_form,
+        delete_product_by_id_form=delete_product_by_id_form
+    )
