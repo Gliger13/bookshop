75f5f14bb9bc495c541beb6cfeb4ec348a060959
Gliger13 <zanevskiyandrey@gmail.com>
2023-03-11 00:14:27 +0100

    bugfix(unit-tests): use JWT token for requests

--- tests/bookshop-unit-tests/bookshop_unit_tests/endpoints/booking/test_ddt_get_valid_booking.py
+++ tests/bookshop-unit-tests/bookshop_unit_tests/endpoints/booking/test_ddt_get_valid_booking.py
@@ -18,5 +18,6 @@ def test_ddt_get_valid_booking(test_data: dict, application_client: Flask):
     """
     booking_id_to_get = test_data["booking_id_to_get"]
     endpoint = f"/api/booking/{booking_id_to_get}"
-    response = application_client.get(endpoint, auth=BasicAuth(**test_data["actor_credentials"]))
+    token_response = application_client.get("/api/generate_token", auth=BasicAuth(**test_data["actor_credentials"]))
+    response = application_client.get(endpoint, headers={"Authorization": f"Bearer {token_response.json['AuthToken']}"})
     soft_check_response_status_code(SimpleResponse(endpoint, "GET", response.status_code), codes.ok)
